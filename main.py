86ae2bea0783290a9d497b6dc7fa6986f3879d4d
yuxstar1444@protonmail.com <yuxstar1444@protonmail.com>
2023-06-22 14:00:48 -0400

    initial setup of pygame

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame as pg
+
+class SoftwareRender: 
+    def __init__(self) -> None:
+        pg.init()
+        self.RES = self.WIDTH, self.HEIGHT = 800, 600
+        self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
+        self.FPS = 60
+        self.screen = pg.display.set_mode(self.RES)
+        self.clock = pg.time.Clock()
+        
+    def draw(self):
+        self.screen.fill(pg.Color('black'))
+    
+    def run(self):
+        while True:
+            self.draw()
+            [exit() for i in pg.event.get() if i.type == pg.QUIT]
+            pg.display.set_caption(str(self.clock.get_fps()))
+            pg.display.flip()
+            self.clock.tick(self.FPS)
+
+if __name__ == '__main__':
+    app = SoftwareRender()
+    app.run() 
